4b52969fbf23407921e478fe0e0efc856d88dc1a
LCHHeluo <jason19991231@gmail.com>
2023-11-30 23:57:58 +0800

    Refactor ConnectCheck()
    Refactor method of ConnectCheck(), remove redundant else, and move object exist or exist not determination together.

--- Board.cs
+++ Board.cs
@@ -142,64 +142,35 @@ namespace Gomoku
             //TODO:用迴圈一次判斷每一顆棋子(pieceArr[x,y])和對應的行、列以及對角有沒有連續重複
             //TODO:檢測範圍是當前棋子的前後4個延伸，但檢測的棋子只要停在當前棋子就好，如果檢測方向已經到當前棋子後還沒有連線，就不可能再連下去了，因為剩下的棋子不足五顆
             //TODO:因此最多檢測五顆就好，每個方向五顆，四個方向檢測20顆最多(正中心)，最少檢測3個方向，檢測3顆(左上角)
-            bool bugLogSwitch = false;
+            bool bugLogSwitch = true;
             for (int i = nodeIdXMin; i <= currentPiece.X; i++)//i是應檢棋子編號
             {
-                if (piecesArr[i, currentPiece.Y] != null)
-                {
-                    BugLog("該行的" + i + "號位子有應檢棋子", bugLogSwitch);
-                    for (int j = i + 1; j <= nodeIdXMax && j <= i+4; j++)//j是正在被比較的棋子的編號
+                for (int j = i + 1; j <= nodeIdXMax && j <= i + 4 ; j++)//j是正在被比較的棋子的編號
+                {  
+                    if (piecesArr[i, currentPiece.Y] != null && piecesArr[j, currentPiece.Y] != null)
                     {
-                        BugLog("該行的" + i + "號應檢棋子準備比較", bugLogSwitch);
-                        if (piecesArr[j, currentPiece.Y] != null)
+                        if (GetPieceType(i, currentPiece.Y) == GetPieceType(j, currentPiece.Y))
                         {
-                            BugLog("該行的" + i + "號應檢棋子的右邊第" + (j - i) + "個位子有棋", bugLogSwitch);
-                            //if (piecesArr[i, currentPiece.Y].GetPieceType() == piecesArr[j, currentPiece.Y].GetPieceType())
-                            if (GetPieceType(i, currentPiece.Y) == GetPieceType(j, currentPiece.Y))
-                            {
-                                BugLog("應檢棋子與應檢棋子的右邊第" + (j - i) + "顆同色", bugLogSwitch);
-                                connectCounter++;
-                                disconnectChecker = false;
-                                BugLog("目前累積連線顆數:" + (1 + connectCounter) + "顆", bugLogSwitch);
-                            }
-                            else
-                            {
-                                BugLog("應檢棋子與應檢棋子的右邊第" + (j - i) + "顆不同色", bugLogSwitch);
-                                connectCounter = 0;
-                                disconnectChecker = true;
-                                BugLog("目前累積連線顆數:" + (1 + connectCounter) + "顆", bugLogSwitch);
-                            }
+                            BugLog("該行的" + i + "號有應檢棋子且它的右邊第" + (j - i) + "個位子有棋", bugLogSwitch);
+                            BugLog("應檢棋子與它的右邊第" + (j - i) + "顆同色", bugLogSwitch);
+                            connectCounter++;
+                            disconnectChecker = false;
+                            BugLog("目前累積連線顆數:" + (1 + connectCounter) + "顆", bugLogSwitch);
                         }
                         else
                         {
-                            BugLog("該行的" + i + "號應檢棋子的右邊第" + (j - i) + "個位子沒有棋子", bugLogSwitch);
-                            connectCounter = 0;
-                            disconnectChecker = true;
+                            BugLog("應檢棋子與它的右邊第" + (j - i) + "顆不同色", bugLogSwitch);
+                            break;
+                        }
+                        if (disconnectChecker != true && connectCounter == 4)
+                        {
+                            BugLog("贏了", bugLogSwitch, true);
+                            break;
                         }
                     }
                 }
-                else
-                {
-                    BugLog("該行的" + i + "號位子沒有應檢棋子", bugLogSwitch);
-                    connectCounter = 0;
-                    disconnectChecker = true;
-                }
-
-                BugLog("該行的" + i + "號應檢棋子判斷完畢", bugLogSwitch);
-                BugLog("該行的" + i + "號應檢棋子總連線顆數:" + (1 + connectCounter) + "顆", bugLogSwitch);
-                if (disconnectChecker != true && connectCounter == 4)
-                {
-                    BugLog("贏了", bugLogSwitch,true);
-                    connectCounter = 0;
-                    disconnectChecker = true;
-                    break;
-                }
-                else
-                {
-                    BugLog("該行的" + i + "號應檢棋子沒有連線", bugLogSwitch);
-                    connectCounter = 0;
-                    disconnectChecker = true;
-                }
+                connectCounter = 0;
+                disconnectChecker = true;
             }
 
             //TODO:每次顏色一樣的話，連線計數器(connectCounter)就+1
